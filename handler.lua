0dec1cc75631e994b0682e3d4d4676b1f0e174e6
XiongMingcai <xmc000@icloud.com>
2024-02-29 02:22:13 +0000

    update handler.lua. 日志添加请求体相应体
    Signed-off-by: XiongMingcai <xmc000@icloud.com>

--- handler.lua
+++ handler.lua
@@ -1,8 +1,7 @@
 local cjson = require "cjson"
 local basic_serializer = require "kong.plugins.log-serializers.basic"
-local body_transformer = require "kong.plugins.response-transformer.body_transformer"
 
-local is_json_body = body_transformer.is_json_body
+
 
 local kong = kong
 local ngx = ngx
@@ -53,36 +52,28 @@ local TcpLogHandler = {
   PRIORITY = 7,
   VERSION = "2.0.1",
 }
-local function parse_body(type, data)
-  if type and data and is_json_body(type) then
-    return cjson.decode(data)
-  end
-end
+
 
 
 function TcpLogHandler:access(conf)
-  if is_json_body(kong.request.get_header("Content-Type")) then
     local ctx = kong.ctx.plugin;
     ctx.request_body = kong.request.get_raw_body();
-  end
 end
 
 
 function TcpLogHandler:body_filter(conf)
-  if is_json_body(kong.response.get_header("Content-Type")) then
-    local ctx = kong.ctx.plugin;
-    local chunk, eof = ngx.arg[1], ngx.arg[2];
-    if not eof then
-      ctx.response_body = (ctx.response_body or "") .. (chunk or "")
-    end
+  local ctx = kong.ctx.plugin;
+  local chunk, eof = ngx.arg[1], ngx.arg[2];
+  if not eof then
+     ctx.response_body = (ctx.response_body or "") .. (chunk or "")
   end
 end
 
 function TcpLogHandler:log(conf)
   local ctx = kong.ctx.plugin;
   local log_obj = basic_serializer.serialize(ngx)
-  log_obj.request.body = parse_body(kong.request.get_header("Content-Type"), ctx.request_body)
-  log_obj.response.body = parse_body(kong.response.get_header("Content-Type"), ctx.response_body)
+  log_obj.request.body = ctx.request_body
+  log_obj.response.body = ctx.response_body
 
 
   local ok, err = timer_at(0, log, conf, log_obj)
